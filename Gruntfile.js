bdff3f2badac8e5b49fecf9fcb3160b9788b1695
leonelfolmer <leonel@leonelfolmer.com>
2015-01-20 12:12:53 -0200

    Removed some bug dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,29 +41,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    concat: {
-      target: {
-        options: {
-          separator: ';'
-        },
-        src: [
-          '<%= config.src %>/scripts/affix.js',
-          '<%= config.src %>/scripts/alert.js',
-          '<%= config.src %>/scripts/button.js',
-          '<%= config.src %>/scripts/carousel.js',
-          '<%= config.src %>/scripts/collapse.js',
-          '<%= config.src %>/scripts/dropdown.js',
-          '<%= config.src %>/scripts/main.js',
-          '<%= config.src %>/scripts/modal.js',
-          '<%= config.src %>/scripts/popover.js',
-          '<%= config.src %>/scripts/scrollspy.js',
-          '<%= config.src %>/scripts/tab.js',
-          '<%= config.src %>/scripts/tooltip.js',
-          '<%= config.src %>/scripts/transition.js'
-        ],
-        dest: '<%= config.dist %>/scripts/bootstrap.js'   
-      }
-    },
     uglify: {
       target: {
         files: [{
@@ -106,10 +83,6 @@ module.exports = function(grunt) {
         src: '<%= config.src %>/fonts/*',
         dest: '<%= config.dist %>/fonts/'
       },
-      etc: {
-        src: '<%= config.src %>/etc/*/*',
-        dest: '<%= config.dist %>/etc/'
-      },
       server: {
         src: 'src/default.js',
         dest: 'dist/default.js'
@@ -117,7 +90,6 @@ module.exports = function(grunt) {
     }  
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
@@ -126,7 +98,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', [
-    'concat',
     'htmlmin',
     'jshint',
     'uglify',
